8a0d50017b8bc796b48b14d3b5cda912175cb511
sameer sapra <sameersapra1@outlook.com>
2019-03-03 23:59:28 -0500

    Added Model Context, All Employees and an Employee View.

---
+++ Northwind/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ModelContext modelContext = new ModelContext();
+            List<Employee> employees = modelContext.Employees.ToList();
+            return View(employees);
+        }
+
+
+        // GET: Employee
+        public ActionResult Details(int id)
+        {
+            ModelContext modelContext = new ModelContext();
+            //List<Employee> employees =  modelContext.Employees;
+            Employee employee = modelContext.Employees.Single(emp => emp.EmployeeID == id);
+
+            return View(employee);
+        }
+    }
+}
---
+++ Northwind/Models/ModelContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Northwind.Models
+{
+    public class ModelContext: DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
